7d2418b592dd562585bbca0885f3e7039cd52314
AlinurAbdyiskakov <abdyiskaovalinur7@gmail.com>
2021-02-19 16:30:47 +0600

    add Addevenlisener scripts.js

---
+++ scripts.js
@@ -0,0 +1,9 @@
+let button = document.querySelector('button');
+let nameSelector = document.querySelector('#name');
+let message = document.querySelector('#message');
+let color = document.querySelector('#color');
+
+
+button.addEventListener('click', function() {
+    console.log(input.value);
+});
