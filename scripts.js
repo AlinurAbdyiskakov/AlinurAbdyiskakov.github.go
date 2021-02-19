4610ddafd32596aa74e448a593d6efcc3c34f8a3
AlinurAbdyiskakov <abdyiskaovalinur7@gmail.com>
2021-02-19 16:39:47 +0600

    add AddEntlisener

--- scripts.js
+++ scripts.js
@@ -1,9 +1,18 @@
-let button = document.querySelector('button');
-let nameSelector = document.querySelector('#name');
-let message = document.querySelector('#message');
-let color = document.querySelector('#color');
+let btn1 = document.querySelector('#btn1')
+let name = document.querySelector('#name')
+let massege = document.querySelector('#massage')
+let color = document.querySelector('#color')
+let chat = document.querySelector('#chat')
 
+btn1.addEventListener("mousedown", function() {
+    let div = document.createElement('div')
+    let span1 = document.createElement('span')
+    let span2 = document.createElement('span')
+    div.append(span1)
+    div.append(span2)
+    chat.append(div)
+    span1.innerText = name.value + ": "
+    span2.innerText = massege.value
+    span1.style.color = color.value
+})
 
-button.addEventListener('click', function() {
-    console.log(input.value);
-});
